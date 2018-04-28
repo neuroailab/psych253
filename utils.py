5a4e43c2b95e5aa33ba7377a992d4f4bd1d9d652
Daniel Yamins <dyamins@gmail.com>
2018-04-28 09:33:03 -0700

    splithalf reliability calculator added to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as stats
 
 
 def featurewise_norm(data, fmean=None, fvar=None):
@@ -25,3 +26,54 @@ def spearman_brown(uncorrected, multiple):
     numerator = multiple * uncorrected
     denominator = 1 + (multiple - 1) * uncorrected
     return numerator / denominator
+
+
+def idfunc(x):
+    return x
+
+
+def pearsonr(a, b):
+    return stats.pearsonr(a, b)[0]
+
+
+def spearmanr(a, b):
+    return stats.spearmanr(a, b)[0]
+
+
+def split_half_correlation(datas_by_trial,
+                           num_splits,
+                           aggfunc=idfunc,
+                           statfunc=pearsonr):
+    """
+    """
+        
+    random_number_generator = np.random.RandomState(seed=0)
+
+    corrvals = []
+    for split_index in range(num_splits):
+        stats1 = []
+        stats2 = []
+        for data in datas_by_trial:
+            #get total number of trials
+            num_trials = data.shape[0]
+
+            #construct a new permutation of the trial indices
+            perm = random_number_generator.permutation(num_trials)
+
+            #take the first num_trials/2 and second num_trials/2 pieces of the data
+            first_half_of_trial_indices = perm[:num_trials / 2]
+            second_half_of_trial_indices = perm[num_trials / 2: num_trials]
+
+            #mean over trial dimension
+            s1 = aggfunc(data[first_half_of_trial_indices].mean(axis=0))
+            s2 = aggfunc(data[second_half_of_trial_indices].mean(axis=0))
+            stats1.extend(s1)
+            stats2.extend(s2)
+        
+        #compute the correlation between the means
+        corrval = statfunc(np.array(stats1), 
+                           np.array(stats2))
+        #add to the list
+        corrvals.append(corrval)
+        
+    return spearman_brown(np.array(corrvals), 2)
