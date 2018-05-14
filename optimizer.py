ab4d4a247600ec951046b653bd7421b2d740d842
Dan Yamins <dyamins@gmail.com>
2018-05-14 14:20:23 -0700

    minor

--- optimizer.py
+++ optimizer.py
@@ -142,77 +142,106 @@ class TF_Optimizer(object):
             optimizer_kwargs = {}
         self.optimizer = optimizer_class(*optimizer_args, 
                                          **optimizer_kwargs)
-        
-    def fit(self, train_data, train_labels):
+
+    def get_data(self, data, labels, shuffle, shuffle_seed, pad):
         if 'data' in self.data_processors:
-            td = self.data_processors['data'](train_data,
+            td = self.data_processors['data'](data,
                                               range(self.batch_size))
         else:
-            td = train_data[:1]
+            td = data[:1]
         fshape = td.shape[1:]
-        self.data_holder = tf.placeholder(shape=(None,) + fshape,
+        if not hasattr(self, 'data_holder'):
+            self.data_holder = tf.placeholder(shape=(None,) + fshape,
                                      dtype=tf.float32,
                                      name='data')
-        if 'labels' in self.data_processors:
-            tl = self.data_processors['labels'](train_labels,
+
+        data_dict = {self.data_holder: data}            
+        if labels is not None:
+            if 'labels' in self.data_processors:
+                tl = self.data_processors['labels'](labels,
                                                 range(self.batch_size))
-        else:
-            tl = train_labels[:1]
-        lshape = tl.shape[1:]
-        self.label_holder = tf.placeholder(shape=(None,) + lshape,
+            else:
+                tl = labels[:1]
+            lshape = tl.shape[1:]
+            if not hasattr(self, 'label_holder'):
+                self.label_holder = tf.placeholder(shape=(None,) + lshape,
                                      dtype=tf.float32,
                                      name='labels')
+            data_dict[self.label_holder] = labels
+ 
+
+        data_processors={}
+        if self.data_processors:
+            if 'data' in self.data_processors:
+                data_processors[self.data_holder] = self.data_processors['data']
+            if labels is not None and 'labels' in self.data_processors:
+                data_processors[self.label_holder] = self.data_processors['labels']
+        data_reader = BatchReader(data_dict=data_dict,
+                                  batch_size=self.batch_size,
+                                  shuffle=shuffle,
+                                  shuffle_seed=shuffle_seed,
+                                  pad=pad,
+                                  processors=data_processors,
+                                  **kwargs)
+        return data_reader
+
+        
+    def fit(self, train_data, train_labels, report_freq=100):
+        if not isinstance(train_data, tf.Tensor):
+            data_reader = self.get_data(train_data,
+                                        train_labels,
+                                        shuffle=self.train_shuffle,
+                                        shuffle_seed=0,
+                                        pad=True)
+        else:
+            data_reader = None
+            self.data_holder = train_data
+            self.label_holder = train_labels
+            
         self.model = self.model_func(self.data_holder,
         			     self.label_holder,
         			     **self.model_kwargs)
         self.loss = self.loss_func(self.model,
                                    self.label_holder,
-                                   **self.loss_kwargs)
-        
+                                   **self.loss_kwargs)        
         self.optimizer_op = self.optimizer.minimize(self.loss)
             
-        data_dict = {self.data_holder: train_data,
-                     self.label_holder: train_labels}
-        data_processors={}
-        if self.data_processors:
-            if 'data' in self.data_processors:
-                data_processors[self.data_holder] = self.data_processors['data']
-            if 'labels' in self.data_processors:
-                data_processors[self.label_holder] = self.data_processors['labels']
-        train_data = BatchReader(data_dict=data_dict,
-                                 batch_size=self.batch_size,
-                                 shuffle=self.train_shuffle,
-                                 shuffle_seed=0,
-                                 pad=True,
-                                 processors=data_processors)
         
         init_op = tf.global_variables_initializer()
         self.sess.run(init_op)
         
         self.losses = []                      
         for i in range(self.train_iterations):
-            data_batch = train_data.next()
-            output = self.sess.run({'opt': self.optimizer_op,
-                                    'loss': self.loss}, 
-                                    feed_dict=data_batch)
-            if i % 100 == 0:
+            if data_reader is not None:
+                data_batch = data_reader.next()
+                output = self.sess.run({'opt': self.optimizer_op,
+                                        'loss': self.loss}, 
+                                        feed_dict=data_batch)
+            else:
+                output = self.sess.run({'opt': self.optimizer_op,
+                                        'loss': self.loss})
+            if i % report_freq == 0:
                 print('iteration %d loss %.3f' % (i, output['loss']))
             self.losses.append(output['loss'])
                 
-    def predict(self, test_data):
-        data_dict = {self.data_holder: test_data}
-        data_processors={}
-        if self.data_processors:
-            if 'data' in self.data_processors:
-                data_processors[self.data_holder] = self.data_processors['data']
-        test_data = BatchReader(data_dict=data_dict,
-                             batch_size=self.batch_size,
-                             shuffle=False,
-                             pad=False,
-                             processors=data_processors)                  
+    def predict(self, test_data, num_batches=None):
+        if not isinstance(test_data, tf.Tensor):
+            data_reader = self.get_data(test_data,
+                                        None,
+                                        shuffle=False,
+                                        shuffle_seed=0,
+                                        pad=False)
+        else:
+            data_reader = None
         preds = []
-        for i in range(test_data.total_batches):
-            data_batch = test_data.get_batch(i)
-            pred_batch = self.sess.run(self.model, feed_dict=data_batch)
+        if num_batches is None:
+            num_batches = test_data.total_batches
+        
+        for i in range(num_batches):
+            if data_reader is not None:
+                data_batch = test_data.get_batch(i)
+                pred_batch = self.sess.run(self.model, feed_dict=data_batch)
+            else:
+                pred_batch = self.sess.run(self.model)
             preds.append(pred_batch)
         return np.row_stack(preds)
